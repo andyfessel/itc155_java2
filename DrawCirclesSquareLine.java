3bbead2a3ef212f0961b4ff7afa1e134c66dacdd
Andy Fessel <38083206+andyfessel@users.noreply.github.com>
2019-04-08 18:51:26 -0700

    Add files via upload

---
+++ DrawCirclesSquareLine.java
@@ -0,0 +1,22 @@
+//Homework Chap3-Ex1 Draw Diagrams
+
+import java.awt.*;
+
+public class DrawCirclesSquareLine {
+
+	public static void main(String[] args) {
+		DrawingPanel panel = new DrawingPanel(220, 150);
+		panel.setBackground(Color.YELLOW);
+				
+		Graphics g = panel.getGraphics();
+		g.setColor(Color.BLUE);
+		g.fillOval(50, 25, 40, 40);
+		g.fillOval(130, 25, 40, 40);
+		g.setColor(Color.RED);
+		g.fillRect(70, 45, 80, 80);
+		g.setColor(Color.BLACK);
+		g.drawLine(70, 85, 150, 85);
+		
+	}
+
+}
