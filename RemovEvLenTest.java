ab683ef71a86458085de3df713563de160295efa
Andy Fessel <38083206+andyfessel@users.noreply.github.com>
2019-04-16 16:31:14 -0700

    Add files via upload

---
+++ RemovEvLenTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RemovEvLenTest {
+
+	@Test
+	public void testDefEven() {
+		ArrayList<String> testlist = new ArrayList<String>();
+		testlist.add("even");
+		(element.length == % 2 = 0));
+		assertEquals(true); 
+		fail("function not correct");
+	}
+
+}
